2dc4dfc4942775437b3fe44b735e0ff854df3570
Samyak Bambole <samyak.bambole07@gmail.com>
2026-08-09 01:15:21 +0530

    bump

--- abc470_c.cpp
+++ abc470_c.cpp
@@ -2,7 +2,9 @@
  * https://atcoder.jp/contests/abc467/tasks/abc470_c
  */
 
-#include <array>
+#include "io.hpp"
+#include "svec.hpp"
+
 #include <cstdint>
 #include <cstdio>
 #include <vector>
@@ -15,20 +17,19 @@ using u64 = std::uint64_t;
 
 constexpr u64 LIMIT = 500000;
 
-u64 n, q;
-std::array<u32, LIMIT> as{};
+clix::svec<u32, LIMIT> as{};
 std::vector<u32> acs;
 
 u32 qi, x, xora;
 
 int main() {
-  std::scanf("%ld %ld", &n, &q);
+  auto [n, q] = io.read<u64, 2>();
 
   while (q-- > 0) {
-    std::scanf("%d", &qi);
+    io.read(&qi);
 
     if (qi == 1) {
-      std::scanf("%d", &x);
+      io.read(&x);
       auto id = x - 1;
 
       if (as[id] == 0) {
--- abc470_d.cpp
+++ abc470_d.cpp
@@ -2,7 +2,9 @@
  * https://atcoder.jp/contests/abc467/tasks/abc470_d
  */
 
-#include <array>
+#include "io.hpp"
+#include "svec.hpp"
+
 #include <cstdint>
 #include <cstdio>
 #include <utility>
@@ -14,43 +16,44 @@ using u32 = std::uint32_t;
 
 constexpr u32 LIMIT = 500000;
 
-u32 n, q;
 u32 qi, x, y;
 u32 f, g;
-std::array<u32, LIMIT + 1> p;
-std::array<u32, LIMIT + 1> pi;
+clix::svec<u32, LIMIT + 1> p;
+clix::svec<u32, LIMIT + 1> pi;
 
 auto *pref = p.data();
 auto *piref = pi.data();
 
 int main() {
-  std::scanf("%d %d", &n, &q);
+  auto [n, q] = io.read<u32, 2>();
 
-  for (u32 i = 1; i <= n; i++) {
-    std::scanf("%d", &p[i]);
-  }
+  io.read(p, n);
 
-  for (u32 i = 1; i <= n; i++) {
-    pi[p[i]] = i;
+  for (u32 i = 0; i < n; i++) {
+    pi[p[i] - 1] = i + 1;
   }
 
   while (q-- > 0) {
-    std::scanf("%d", &qi);
+    io.read(&qi);
 
     if (qi == 1) {
-      std::scanf("%d %d", &x, &y);
+      auto [x, y] = io.read<u32, 2>();
+      x--;
+      y--;
 
-      f = pref[x], g = pref[y];
+      f = pref[x];
+      g = pref[y];
 
       std::swap(pref[x], pref[y]);
 
-      std::swap(piref[f], piref[g]);
+      piref[f - 1] = y + 1;
+      piref[g - 1] = x + 1;
     } else {
       std::swap(pref, piref);
     }
   }
 
-  for (auto i = 1; i <= n; i++) {
+  for (auto i = 0; i < n; i++) {
     std::printf("%d ", pref[i]);
   }
 
